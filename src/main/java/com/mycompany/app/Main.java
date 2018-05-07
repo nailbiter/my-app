4bd0b5c77bc957879df10bebb2c5f72038fbeca2
レオンチエフ　アレックス <aloz1991@gmail.com>
2018-05-08 00:06:52 +0900

    final version

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -42,15 +42,25 @@ import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
 public class Main{
   static IMAPDemo id = null;
   static SlackSession session = null;
+  static SlackUser sb = null;
   public static void main(String[] args) throws Exception
   {
     id = new IMAPDemo();
-    String token = KeyRing.getWebToken();
+    String token = KeyRing.getBotWebToken();
 		//System.out.print(String.format("secret: %s\n",token));
 		session = SlackSessionFactory.createWebSocketSlackSession(token);
 		session.connect();
 		//SlackChannel channel = session.findChannelByName("general"); //make sure bot is a member of the channel.
 		//session.sendMessage(channel, "hi im a bot" );
+
+    System.out.println("users");
+    for(SlackUser su : session.getUsers())
+      System.out.println("\t"+su.toString());
+    System.out.println("bots");
+    for(SlackBot su : session.getBots())
+      System.out.println("\t"+su.toString());
+    sb = session.findUserById("UAKCACQKC");
+    System.out.println("\tbot: "+sb.toString());
 
 		registeringAListener(session);
 		for(;;);
@@ -63,19 +73,31 @@ public class Main{
           @Override
           public void onEvent(SlackMessagePosted event, SlackSession session)
           {
-              SlackChannel channelOnWhichMessageWasPosted = event.getChannel();
+            SlackChannel channelOnWhichMessageWasPosted = event.getChannel();
+            try{
+
               String messageContent = event.getMessageContent();
               String channelName = channelOnWhichMessageWasPosted.getName();
               SlackUser messageSender = event.getSender();
-							System.out.println(String.format("got message %s on channel %s by user %s!",
-							       messageContent,channelName,messageSender.getUserName()));
-              if(!true)
+              String senderUserName = messageSender.getUserName();
+
+							/*System.out.println(String.format("got message %s on channel %s by user %s,bot %s!: %s",
+							       messageContent,channelName,messageSender.getUserName(),
+                     event.getBot(),event.toString()));*/
+              if(senderUserName.equals(KeyRing.getUserName()))
+                session.sendMessage(channelOnWhichMessageWasPosted,
+                  id.reply(messageContent));
+            }
+            catch(Exception e)
+            {
               session.sendMessage(channelOnWhichMessageWasPosted,
-                String.format("was posted: %s",messageContent));
+                String.format("e: %s",e.toString()));
+            }
           }
       };
       //add it to the session
       session.addMessagePostedListener(messagePostedListener);
+      //session.sendMessage(session.findChannelByName("general"),"hi");
 
       //that's it, the listener will get every message post events the bot can get notified on
       //(IE: the messages sent on channels it joined or sent directly to it)
